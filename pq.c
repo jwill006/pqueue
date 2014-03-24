4b504b8f8e19151bf3cfbc783ccbddd639eee785
Jordan Williams <jwill006@yahoo.com>
2014-03-24 14:15:19 -0500

    annotations

--- pq.c
+++ pq.c
@@ -25,23 +25,28 @@ struct pq_struct {
 
 /************ Helper Functions ************/
 
+// Returns 1 if priority queue is NULL or empty -- O(1)
 static int is_empty(PQ_PTR pq) {
     if (pq==NULL) return 1;
     return pq->size==0;
 }
 
+// Returns 1 if priority queue is not empty -- O(1)
 static int is_not_empty(void *s) {
     return is_empty(s)==0;
 }
 
+// Returns 1 if node is NULL -- O(1)
 static int is_null(nodePtr r) {
     return r==NULL;
 }
 
+// Returns 1 if node is not NULL -- O(1)
 static int is_not_null(nodePtr r) {
     return is_null(r)==0;
 }
 
+// Prints an error message and returns 0 -- O(1)
 static int id_out_of_bounds(PQ_PTR pq, int id) {
     if ((id < 0) || (id > pq->capacity)) {
         fprintf(stderr, "ID is out of bounds (0-%d)\n", pq->capacity-1);
@@ -50,27 +55,31 @@ static int id_out_of_bounds(PQ_PTR pq, int id) {
     return 0;
 }
 
+// Prints an error message and returns 0 -- O(1)
 static int not_a_queue() {
     fprintf(stderr, "Not a queue\n");
     return 0;
 }
 
+// Returns priority of a node -- O(1)
 static double priority(nodePtr r) {
-    if (is_null(r)) return -999;
+    if (is_null(r)) return -999; // Should never happen
     return r->priority;
 }
 
+// Returns node from id index -- O(1)
 static nodePtr retrieve_by_id(PQ_PTR pq, int id) {
     return pq->ids[id+1];
 }
 
-// Given a node, return its parent
+// Returns parent node or NULL -- O(1)
 static nodePtr retrieve_parent(PQ_PTR pq, nodePtr r) {
     if (is_null(r)) return NULL;
     int parent_pos = r->heap_pos/2;    
     return (parent_pos>0) ? pq->heap[parent_pos] : NULL;
 }
 
+// Returns left child or NULL -- O(1)
 static nodePtr retrieve_left_child(PQ_PTR pq, nodePtr r) {
     if (is_null(r)) return NULL;
     
@@ -78,6 +87,7 @@ static nodePtr retrieve_left_child(PQ_PTR pq, nodePtr r) {
     return (left_pos<1 || left_pos>pq->size) ? NULL : pq->heap[left_pos];
 }
 
+// Returns right child or NULL -- O(1)
 static nodePtr retrieve_right_child(PQ_PTR pq, nodePtr r) {
     if (is_null(r)) return NULL;
     
@@ -85,16 +95,19 @@ static nodePtr retrieve_right_child(PQ_PTR pq, nodePtr r) {
     return (right_pos<1 || right_pos>pq->size) ? NULL : pq->heap[right_pos];
 }
 
+// Returns last element in priority queue array -- O(1)
 static nodePtr retrieve_heap_tail(PQ_PTR pq) {
     if (is_empty(pq)) return NULL;
     return pq->heap[pq->size];
 }
 
+// Returns 1 if an id is in the priority queue -- O(1)
 static int is_in_heap(PQ_PTR pq, int id) {
     if (is_empty(pq) || id_out_of_bounds(pq,id) || (retrieve_by_id(pq,id)->heap_pos == -1)) return 0;
     return 1;
 }
 
+// Returns 1 if an id is not in the priority queue -- O(1)
 static int is_not_in_heap(PQ_PTR pq, int id) {
     return is_in_heap(pq,id)==0;
 }
@@ -130,6 +143,7 @@ void pq_preorder(PQ_PTR pq) {
 
 /************ Heap Functions ************/
 
+// Returns a new node with given ID -- O(1)
 static nodePtr create_node(int id) {
     nodePtr tmp = malloc (sizeof(NODE));
     tmp->id = id;
@@ -138,20 +152,24 @@ static nodePtr create_node(int id) {
     return tmp;
 }
 
+// Returns 1 priority queue is a MIN heap -- O(1)
 static int is_min_heap(PQ_PTR pq) {
     return pq->type==MIN;
 }
 
+// Returns 1 priority queue is a MAX heap -- O(1)
 static int is_max_heap(PQ_PTR pq) {
     return pq->type==MAX;
 }
 
+// Returns 1 if a node with ID is in priority queue -- O(1)
 static int heap_contains(PQ_PTR pq, int id, double *priority) {
     if (is_not_in_heap(pq,id)) return 0;
     if (priority!=NULL) (*priority) = retrieve_by_id(pq,id)->priority;
     return 1;
 }
 
+// Returns address of array with initialized nodes -- O(N)
 static nodePtr *init_ids(PQ_PTR pq) {
     nodePtr *arr = malloc (sizeof(nodePtr) * (pq->capacity+1));
     
@@ -163,6 +181,7 @@ static nodePtr *init_ids(PQ_PTR pq) {
     return arr;
 }
 
+// Returns address of array for heap implementation -- O(N)
 static nodePtr *init_heap(PQ_PTR pq) {
     nodePtr *arr = malloc (sizeof(nodePtr) * (pq->capacity+1));
     
@@ -174,6 +193,7 @@ static nodePtr *init_heap(PQ_PTR pq) {
     return arr;
 }
 
+// Validates node-parent relation is consistent with heap properities -- O(logN)
 static int percolate_up(PQ_PTR pq, nodePtr r) {
     nodePtr parent = retrieve_parent(pq,r);
     
@@ -189,6 +209,7 @@ static int percolate_up(PQ_PTR pq, nodePtr r) {
     return percolate_up(pq,r);
 }
 
+// Validates node-children relations are consistent with heap properities -- O(logN)
 static int percolate_down(PQ_PTR pq, nodePtr r) {
     nodePtr left_child = retrieve_left_child(pq,r);
     nodePtr right_child = retrieve_right_child(pq,r);
@@ -221,38 +242,42 @@ static int percolate_down(PQ_PTR pq, nodePtr r) {
 
 /************ Required Functions ************/
 
+// Returns capacity of priority queue -- O(1)
 int pq_capacity(PQ_PTR pq) {
     return is_empty(pq) ? 0 : pq->capacity;
 }
 
+// Returns current size of priority queue -- O(1)
 int pq_size(PQ_PTR pq) {
     return is_empty(pq) ? 0 : pq->size;
 }
 
+// Returns 1 if process with ID is in priority queue -- O(1)
 int pq_contains(PQ_PTR pq, int id) {
     return heap_contains(pq, id, NULL);
 }
 
-// 
+// Returns priority of a process with ID -- O(1)
 int pq_get_priority(PQ_PTR pq, int id, double *priority) {
     return heap_contains(pq, id, priority);
 }
 
-// Returns a pointer to a newly initialized max/min heap
+// Returns a pointer to a newly initialized priority queue -- O(N)
 PQ_PTR pq_create(int capacity, int min_heap) {
     PQ_PTR pq = malloc (sizeof(struct pq_struct));
     pq->capacity = capacity;
     pq->type = (min_heap ? MIN : MAX);
     pq->size = 0;
-    pq->heap = init_heap(pq);    
-    pq->ids = init_ids(pq);
+    pq->heap = init_heap(pq); // -- O(N)
+    pq->ids = init_ids(pq);   // -- O(N)
     return pq;
 }
 
+// Deallocates priority queue -- O(1)
 void pq_free(PQ_PTR pq) {
     int i, capacity=pq->capacity;
     
-    for (i=1; i<=capacity; i++) {
+    for (i=0; i<capacity; i++) {
         nodePtr r = retrieve_by_id(pq,i);
         r->id = 0;
         r->priority = 0;
@@ -267,17 +292,19 @@ void pq_free(PQ_PTR pq) {
     free(pq);
 }
 
+// Insert process with ID and PRIORITY into priority queue -- O(logN)
 int pq_insert(PQ_PTR pq, int id, double priority) {
-    if (heap_contains(pq, id, NULL) || id_out_of_bounds(pq,id)) return 0;
+    if (heap_contains(pq, id, NULL) || id_out_of_bounds(pq,id)) return 0;  // -- O(1)
     retrieve_by_id(pq,id)->priority = priority;
     int size = ++(pq->size);
     
-    pq->heap[size] = retrieve_by_id(pq,id);
+    pq->heap[size] = retrieve_by_id(pq,id); // -- O(1)
     pq->heap[size]->heap_pos = size;
-    percolate_up(pq, pq->heap[size]);
+    percolate_up(pq, pq->heap[size]);       // -- O(logN)
     return 1;
 }
 
+// Removes a process with ID from priority queue -- O(logN)
 int pq_remove_by_id(PQ_PTR pq, int id) {
     if (heap_contains(pq, id, NULL) == 0) return 0;
     
@@ -295,33 +322,24 @@ int pq_remove_by_id(PQ_PTR pq, int id) {
     --(pq->size);
     
     if (is_min_heap(pq)) {
-        if (new_priority > old_priority) percolate_down(pq, replacement);
-        else percolate_up(pq, replacement);
+        if (new_priority > old_priority) percolate_down(pq, replacement); // -- O(logN)
+        else percolate_up(pq, replacement); // -- O(logN)
     }
     else {
-        if (new_priority < old_priority) percolate_down(pq, replacement);
-        else percolate_down(pq, replacement);
+        if (new_priority < old_priority) percolate_down(pq, replacement); // -- O(logN)
+        else percolate_up(pq, replacement); // -- O(logN)
     }
     
     return 1;
 }
 
+// Removes the process with the highest priority in the queue -- O(logN)
 int pq_delete_top(PQ_PTR pq, int *id, double *priority) {
     if (is_empty(pq)) return 0;
-    
-    nodePtr target = pq->heap[1];
-    if (id!=NULL) *id = target->id;
-    if (priority!=NULL) *priority = target->priority;
-    target->heap_pos = -1;
-    target->priority = 0;
-    
-    pq->heap[1] = retrieve_heap_tail(pq);
-    pq->heap[1]->heap_pos = 1;
-    pq->heap[pq_size(pq)] = NULL;
-    --(pq->size);
-    percolate_down(pq, pq->heap[1]);
+    return pq_remove_by_id(pq, pq->heap[1]->id);
 }
 
+// Updates the priority of process with ID -- O(logN)
 int pq_change_priority(PQ_PTR pq, int id, double new_priority) {
     if (heap_contains(pq, id, NULL) == 0) return 0;
     
@@ -342,7 +360,8 @@ int pq_change_priority(PQ_PTR pq, int id, double new_priority) {
     return 1;
 }
 
-int test_heap_associations(PQ_PTR pq, nodePtr r) {
+// Validates heap from top-down
+int test_heap_associations_helper(PQ_PTR pq, nodePtr r) {
     if (is_null(r)) return 1;
     nodePtr left_child = retrieve_left_child(pq,r);
     nodePtr right_child = retrieve_right_child(pq,r);
@@ -350,31 +369,36 @@ int test_heap_associations(PQ_PTR pq, nodePtr r) {
     if (is_not_null(left_child))
         pq->type==MIN ? assert(priority(r) <= priority(left_child)) : assert(priority(r) >= priority(left_child));
     
-    test_heap_associations(pq, left_child);
+    test_heap_associations_helper(pq, left_child);
     
     if (is_not_null(right_child))
         pq->type==MIN ? assert(priority(r) <= priority(right_child)) : assert(priority(r) >= priority(right_child));
     
-    test_heap_associations(pq, right_child);
+    test_heap_associations_helper(pq, right_child);
 }
 
-
-int main() {
+// Random test generator for priority queue
+int test_heap_associations() {
     srand(time(NULL));
-    int i, size = 20000;
-    PQ_PTR pq = pq_create(size, 0);
+    int i, size = 1000, psize = size*5;
+    PQ_PTR pq = pq_create(size, 1);
     
     for (i=0; i<size; i++) {
-        pq_insert(pq, i, rand()%size);
-        test_heap_associations(pq, pq->heap[1]);
+        pq_insert(pq, i, rand()%psize);
+        test_heap_associations_helper(pq, pq->heap[1]);
     }
     
-    for (i=0; i<size; i++) {
-        pq_delete_top(pq, NULL, NULL);
-        test_heap_associations(pq, pq->heap[1]);
+    for (i=size-1; i>=0; i--) {
+        pq_remove_by_id(pq,i);
+        test_heap_associations_helper(pq, pq->heap[1]);
     }
     
     pq_free(pq);
     
+    return 1;
+}
+
+int main () {
+    test_heap_associations();
     return 1;
 }
